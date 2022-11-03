8fd7ae107189465eaecd9376c3f289c7518a6b3e
Ian Geraldi Cordeiro <iangc49@gmail.com>
2022-11-03 12:32:28 -0300

    C major usando enum de nota

--- create_C_major.py
+++ create_C_major.py
@@ -1,24 +1,25 @@
 from mido import Message, MidiFile, MidiTrack
+from note import Note
 mid = MidiFile()
 track = MidiTrack()
 mid.tracks.append(track)
 
 
-#está funcionando, mas tá soando harpejado
-
-
 track.append(Message('program_change', program=12, time=0))
 
-#c4
-track.append(Message('note_on', note=64, velocity=64, time=0))
-track.append(Message('note_off', note=64, velocity=127, time=32))
 
-#e4
-track.append(Message('note_on', note=60, velocity=64, time=0))
-track.append(Message('note_off', note=60, velocity=127, time=32))
+track.append(Message('note_on', note= Note.C4, velocity=64, time=0))
 
-#g4
-track.append(Message('note_on', note=67, velocity=64, time=0))
-track.append(Message('note_off', note=67, velocity=127, time=32))
+track.append(Message('note_on', note= Note.E4, velocity=64, time=0))
+
+track.append(Message('note_on', note=Note.G4, velocity=64, time=0))
+
+
+track.append(Message('note_off', note=Note.C4, velocity=127, time=1000))
+track.append(Message('note_off', note=Note.E4, velocity=127, time=0))
+track.append(Message('note_off', note=Note.G4, velocity=127, time=0))
+
+track.append(Message('note_on', note=Note.Ab4, velocity=64, time=0))
+track.append(Message('note_off', note=Note.Ab4, velocity=64, time=0))
 
 mid.save('c_major.mid')
