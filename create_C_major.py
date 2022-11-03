326ef1e6a7ff61d451fadb88efff2f5ed084100e
Ian Geraldi Cordeiro <iangc49@gmail.com>
2022-11-03 14:04:56 -0300

    Decrescendo usando o Velocity.py

--- create_C_major.py
+++ create_C_major.py
@@ -1,6 +1,7 @@
 from mido import Message, MidiFile, MidiTrack
 from note import Note
 from tempo import Tempo
+from velocity import Velocity
 mid = MidiFile()
 track = MidiTrack()
 mid.tracks.append(track)
@@ -10,20 +11,20 @@ tempo = Tempo(120)
 track.append(Message('program_change', program=12, time=0))
 
 
-track.append(Message('note_on', note= Note.C4, velocity=64, time=tempo.Time(1)))
+track.append(Message('note_on', note= Note.C4, velocity=Velocity.ffff, time=tempo.Time(1)))
 
-track.append(Message('note_on', note= Note.E4, velocity=64, time=tempo.Time(1)))
+track.append(Message('note_on', note= Note.E4, velocity=Velocity.ff, time=tempo.Time(1)))
 
-track.append(Message('note_on', note=Note.G4, velocity=64, time=tempo.Time(1)))
+track.append(Message('note_on', note=Note.G4, velocity=Velocity.mf, time=tempo.Time(1)))
 
 track.append(Message('note_off', note=Note.C4, velocity=127, time=0))
 track.append(Message('note_off', note=Note.E4, velocity=127, time=0))
 track.append(Message('note_off', note=Note.G4, velocity=127, time=0))
 
-track.append(Message('note_on', note= Note.E4, velocity=64, time=tempo.Time(1)))
+track.append(Message('note_on', note= Note.E4, velocity=Velocity.p, time=tempo.Time(1)))
 
 
-track.append(Message('note_on', note= Note.C4, velocity=64, time=tempo.Time(1)))
+track.append(Message('note_on', note= Note.C4, velocity=Velocity.ppp, time=tempo.Time(1)))
 
 
 mid.save('c_major.mid')
