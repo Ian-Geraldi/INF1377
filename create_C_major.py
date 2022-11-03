d46333d2139d80d19a236ac8b80c56601e245902
Ian Geraldi Cordeiro <iangc49@gmail.com>
2022-11-03 14:01:46 -0300

    C major harpejado usando o Tempo.py

--- create_C_major.py
+++ create_C_major.py
@@ -1,25 +1,29 @@
 from mido import Message, MidiFile, MidiTrack
 from note import Note
+from tempo import Tempo
 mid = MidiFile()
 track = MidiTrack()
 mid.tracks.append(track)
 
+tempo = Tempo(120)
 
 track.append(Message('program_change', program=12, time=0))
 
 
-track.append(Message('note_on', note= Note.C4, velocity=64, time=0))
+track.append(Message('note_on', note= Note.C4, velocity=64, time=tempo.Time(1)))
 
-track.append(Message('note_on', note= Note.E4, velocity=64, time=0))
+track.append(Message('note_on', note= Note.E4, velocity=64, time=tempo.Time(1)))
 
-track.append(Message('note_on', note=Note.G4, velocity=64, time=0))
+track.append(Message('note_on', note=Note.G4, velocity=64, time=tempo.Time(1)))
 
-
-track.append(Message('note_off', note=Note.C4, velocity=127, time=1000))
+track.append(Message('note_off', note=Note.C4, velocity=127, time=0))
 track.append(Message('note_off', note=Note.E4, velocity=127, time=0))
 track.append(Message('note_off', note=Note.G4, velocity=127, time=0))
 
-track.append(Message('note_on', note=Note.Ab4, velocity=64, time=0))
-track.append(Message('note_off', note=Note.Ab4, velocity=64, time=0))
+track.append(Message('note_on', note= Note.E4, velocity=64, time=tempo.Time(1)))
+
+
+track.append(Message('note_on', note= Note.C4, velocity=64, time=tempo.Time(1)))
+
 
 mid.save('c_major.mid')
