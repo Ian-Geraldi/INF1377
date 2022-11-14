bf6557293d911b23b844f16a508ea8ea7c7b885a
Ian Geraldi Cordeiro <iangc49@gmail.com>
2022-11-14 15:56:45 -0300

    Mudança de note para pitch

--- create_C_major.py
+++ create_C_major.py
@@ -1,5 +1,5 @@
 from mido import Message, MidiFile, MidiTrack
-from note import Note
+from pitch import Pitch
 from tempo import Tempo
 from velocity import Velocity
 mid = MidiFile()
@@ -11,20 +11,20 @@ tempo = Tempo(120)
 track.append(Message('program_change', program=12, time=0))
 
 
-track.append(Message('note_on', note= Note.C4, velocity=Velocity.ffff, time=tempo.Time(1)))
+track.append(Message('note_on', note= Pitch.C4, velocity=Velocity.ffff, time=tempo.Time(10)))
 
-track.append(Message('note_on', note= Note.E4, velocity=Velocity.ff, time=tempo.Time(1)))
+track.append(Message('note_on', note= Pitch.E4, velocity=Velocity.ff, time=tempo.Time(1)))
 
-track.append(Message('note_on', note=Note.G4, velocity=Velocity.mf, time=tempo.Time(1)))
+track.append(Message('note_on', note=Pitch.G4, velocity=Velocity.mf, time=tempo.Time(1)))
 
-track.append(Message('note_off', note=Note.C4, velocity=127, time=0))
-track.append(Message('note_off', note=Note.E4, velocity=127, time=0))
-track.append(Message('note_off', note=Note.G4, velocity=127, time=0))
+track.append(Message('note_off', note=Pitch.C4, velocity=127, time=0))
+track.append(Message('note_off', note=Pitch.E4, velocity=127, time=0))
+track.append(Message('note_off', note=Pitch.G4, velocity=127, time=0))
 
-track.append(Message('note_on', note= Note.E4, velocity=Velocity.p, time=tempo.Time(1)))
+track.append(Message('note_on', note= Pitch.E4, velocity=Velocity.p, time=tempo.Time(1)))
 
 
-track.append(Message('note_on', note= Note.C4, velocity=Velocity.ppp, time=tempo.Time(1)))
+track.append(Message('note_on', note= Pitch.C4, velocity=Velocity.ppp, time=tempo.Time(1)))
 
 
 mid.save('c_major.mid')
